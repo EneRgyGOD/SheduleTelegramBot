fe794a6171b9227cf4277d170b1ff7e1927a9714
EneRgy_GOD <maximfedorko@gmail.com>
2021-09-09 20:07:04 +0300

    Add project files.

---
+++ SheduleTelegramBot/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace SheduleTelegramBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.UserList = new System.Windows.Forms.ListBox();
+            this.sendTextBox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Listening";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.StartButtonClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(129, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Listening";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.StopButtonClick);
+            // 
+            // UserList
+            // 
+            this.UserList.FormattingEnabled = true;
+            this.UserList.ItemHeight = 16;
+            this.UserList.Items.AddRange(new object[] {
+            "Misha",
+            "grisha",
+            "galya"});
+            this.UserList.Location = new System.Drawing.Point(255, 14);
+            this.UserList.Name = "UserList";
+            this.UserList.Size = new System.Drawing.Size(56, 116);
+            this.UserList.TabIndex = 2;
+            this.UserList.SelectedIndexChanged += new System.EventHandler(this.ChangedTargetUser);
+            // 
+            // sendTextBox
+            // 
+            this.sendTextBox.Location = new System.Drawing.Point(14, 64);
+            this.sendTextBox.Name = "sendTextBox";
+            this.sendTextBox.Size = new System.Drawing.Size(226, 22);
+            this.sendTextBox.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 92);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 24);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SendButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.sendTextBox);
+            this.Controls.Add(this.UserList);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox UserList;
+        private System.Windows.Forms.TextBox sendTextBox;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+
+     
+    }
+}
+
---
+++ SheduleTelegramBot/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace SheduleTelegramBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private static string token { get; set; } = "1990617206:AAFDXPAOm8RvRMObS8aE4BbGwdzRB9v35Ug";
+        private static TelegramBotClient client;
+
+        private static long CurrentUser = 0;
+        private void StartButtonClick(object sender, EventArgs e)
+        {
+
+            client = new TelegramBotClient(token);
+            client.StartReceiving();
+            client.OnMessage += OnMessageHandler;
+        }
+        private static void OnMessageHandler(object sender, MessageEventArgs e)
+        {
+            var msg = e.Message;
+            if (msg.Text == "bruh")
+            {
+                client.SendTextMessageAsync(e.Message.Chat.Id, "moment");
+            }
+            CurrentUser = e.Message.Chat.Id;
+
+        }
+
+        private void StopButtonClick(object sender, EventArgs e)
+        {
+            client.StopReceiving();
+        }
+
+        
+
+        private void ChangedTargetUser(object sender, System.EventArgs e)
+        {
+            
+        }
+        
+        private void SendButtonClick(object sender, System.EventArgs e)
+        {
+            client.SendTextMessageAsync(CurrentUser, sendTextBox.Text);
+        }
+    }
+}
+
+
